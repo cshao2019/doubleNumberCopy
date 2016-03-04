6c118fc1392573ec83c8ecaac2301a725d6bf2a1
Christine <Christine@Christines-MacBook-Air.local>
2016-03-04 11:10:28 -0500

    With the try

--- doubleNumberCopy.py
+++ doubleNumberCopy.py
@@ -1,16 +1,15 @@
 number = raw_input("What number should I double? ")
 
-while number != "":
+notYetConverted = True
+
+while notYetConverted:
 	try:
-#Try block is only for exception causers- if the user messes up, not programmer. Put as little as possbile
 		number = float(number)
-	except ValueError: #susceptible that there will be an exception- put it under except
-		print("Sorry, that is not a number.")
-		number = raw_input("Try again: ")
-
+		
+	except ValueError:
+		number = input("Sorry that is not a number, try again. ")
+		
 	else:
-#This will run in any case
-		print("Double that is {}. ".format(number * 2))
-		number = ""
+		notYetConverted = False
 
-#want to use it only when the user will cause an error
+print("Double that is {}. ".format(number * 2))
